4da6ec8c0b64275c873ac41ce6efb7b815d0e647
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2018-06-05 22:02:41 +0100

    Implement query-param based blob store

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
 
+
+const redisClient = require('./redis-client');
+
+app.get('/store/:key', async (req, res) => {
+  const { key } = req.params;
+  const value = req.query;
+  await redisClient.setAsync(key, JSON.stringify(value));
+  return res.send('Success');
+});
+
+app.get('/:key', async (req, res) => {
+  const { key } = req.params;
+  const rawData = await redisClient.getAsync(key);
+  return res.json(JSON.parse(rawData));
+});
+
 app.get('/', (req, res) => {
   return res.send('Hello world');
 });
